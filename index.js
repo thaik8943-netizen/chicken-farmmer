cb50100d59e54132a3e9f4a896c087a53e080d00
thaik8943-netizen <thaik8943@gmail.com>
2026-05-02 01:41:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1858,6 +1858,8 @@ if (msg.content === ":help") {
     });
 }
 });
+// ... (Giữ nguyên phần đầu cho đến hết hàm editDistance cũ của ông)
+
 function getSimilarity(s1, s2) {
     let longer = s1.toLowerCase();
     let shorter = s2.toLowerCase();
@@ -1873,20 +1875,25 @@ function getSimilarity(s1, s2) {
 function editDistance(s1, s2) {
     s1 = s1.toLowerCase();
     s2 = s2.toLowerCase();
-    let costs = [];
+    let costs = new Array();
     for (let i = 0; i <= s1.length; i++) {
         let lastValue = i;
         for (let j = 0; j <= s2.length; j++) {
-            if (i === 0) costs[j] = j;
-            else if (j > 0) {
-                let newValue = costs[j - 1];
-                if (s1.charAt(i - 1) !== s2.charAt(j - 1))
-                    newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
-                costs[j - 1] = lastValue;
-                lastValue = newValue;
+            if (i == 0)
+                costs[j] = j;
+            else {
+                if (j > 0) {
+                    let newValue = costs[j - 1];
+                    if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                        newValue = Math.min(Math.min(newValue, lastValue),
+                            costs[j]) + 1;
+                    costs[j - 1] = lastValue;
+                    lastValue = newValue;
+                }
             }
         }
-        if (i > 0) costs[s2.length] = lastValue;
+        if (i > 0)
+            costs[s2.length] = lastValue;
     }
     return costs[s2.length];
 }
