e1e377f0b2bd54399fc251cd6577b5108a6e94e4
thaik8943-netizen <thaik8943@gmail.com>
2026-05-02 01:34:04 +0700

    Fix date comparison and improve editDistance function

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ client.on("messageCreate", async (msg) => {
     const now = Date.now();
     const today = new Date().setHours(0, 0, 0, 0);
 
-    if (u.lastEatReset !== today) { 
+    if (new Date(u.lastEatReset).getTime() !== today.getTime()) {
         u.eatToday = 0; 
         u.lastEatReset = today; 
     }
@@ -1873,19 +1873,17 @@ function getSimilarity(s1, s2) {
 function editDistance(s1, s2) {
     s1 = s1.toLowerCase();
     s2 = s2.toLowerCase();
-    let costs = new Array();
+    let costs = [];
     for (let i = 0; i <= s1.length; i++) {
         let lastValue = i;
         for (let j = 0; j <= s2.length; j++) {
-            if (i == 0) costs[j] = j;
-            else {
-                if (j > 0) {
-                    let newValue = costs[j - 1];
-                    if (s1.charAt(i - 1) != s2.charAt(j - 1))
-                        newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
-                    costs[j - 1] = lastValue;
-                    lastValue = newValue;
-                }
+            if (i === 0) costs[j] = j;
+            else if (j > 0) {
+                let newValue = costs[j - 1];
+                if (s1.charAt(i - 1) !== s2.charAt(j - 1))
+                    newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+                costs[j - 1] = lastValue;
+                lastValue = newValue;
             }
         }
         if (i > 0) costs[s2.length] = lastValue;
