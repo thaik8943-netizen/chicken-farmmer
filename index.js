36e6da32a596fd7863ea2c6097229403d4c9307f
thaik8943-netizen <thaik8943@gmail.com>
2026-05-02 00:02:39 +0700

    Implement string similarity and edit distance functions
    Added similarity and edit distance functions for string comparison.

--- index.js
+++ index.js
@@ -1802,6 +1802,7 @@ if (msg.content === ":help") {
 }
 }); 
 // Dán đoạn này vào cuối file, dưới tất cả các lệnh khác
+// --- HÀM HỖ TRỢ NHẬN DIỆN TÊN GÀ ---
 function getSimilarity(s1, s2) {
     let longer = s1.toLowerCase();
     let shorter = s2.toLowerCase();
@@ -1817,14 +1818,27 @@ function getSimilarity(s1, s2) {
 function editDistance(s1, s2) {
     s1 = s1.toLowerCase();
     s2 = s2.toLowerCase();
-    let costs = new Array();
+    let costs = [];
     for (let i = 0; i <= s1.length; i++) {
         let lastValue = i;
         for (let j = 0; j <= s2.length; j++) {
-            if (i == 0) costs[j] = j;
-            else {
+            if (i === 0) {
+                costs[j] = j;
+            } else {
                 if (j > 0) {
                     let newValue = costs[j - 1];
-                    if (s1.charAt(i - 1) != sNormally I can help with things like this, but I don't seem to have access to that content. You can try again or ask me for something else.
+                    if (s1.charAt(i - 1) !== s2.charAt(j - 1)) {
+                        newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+                    }
+                    costs[j - 1] = lastValue;
+                    lastValue = newValue;
+                }
+            }
+        }
+        if (i > 0) costs[s2.length] = lastValue;
+    }
+    return costs[s2.length];
+}
 
+// --- KHỞI CHẠY BOT ---
 client.login(process.env.TOKEN);
